eced28018dd1afdce2738aec614b8e775179706a
zzatworq <zzatworq@gmail.com>
2026-09-10 17:51:50 +0500

    refactor: replace monolithic monitor home route

--- src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { AppShell } from "@/components/monitor/app-shell";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <AppShell />;
-}
