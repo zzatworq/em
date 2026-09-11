90fd70c1264d5aa41b33454a7a97d6b2cbc17fb7
zzatworq <zzatworq@gmail.com>
2026-09-11 08:47:32 +0500

    ui: close collection popup after successful save

--- src/components/monitor/history-view.tsx
+++ src/components/monitor/history-view.tsx
@@ -143,6 +143,7 @@ export function HistoryView() {
             setRaw("");
             setBill("");
             setPayment("");
+            setCollectionOpen(false);
           }}
         >
           <div>
