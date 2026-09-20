f5df0329d14f81bc09f1614b2ffe5ec677bab2b1
zzatworq <zzatworq@gmail.com>
2026-09-20 22:18:18 +0500

    Migrate restored meter tariffs with correct loads

--- src/store/monitor.ts
+++ src/store/monitor.ts
@@ -34,8 +34,8 @@ export const useMonitor = create<State>()((set, get) => ({
     set({
       ...data,
       collections,
-      tariff1: normalizeTariff(data.tariff1),
-      tariff2: normalizeTariff(data.tariff2),
+      tariff1: normalizeTariff(data.tariff1, 3),
+      tariff2: normalizeTariff(data.tariff2, 2),
       history: syncCollectionHistory(correctedHistory, collections),
       hydrated: true,
       dirty: corrected,
